f19c171ca8d011353e7b16b277509ee292fc3c49
Clay <vapephotog@gmail.com>
2016-08-09 17:28:30 -0500

    Initial Push
    The beginings of a website for a project proposal.

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.set('view engine', 'ejs');
+//Sets the view engine to ejs for partials
+
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+app.get('/equipment', function(req, res) {
+    res.render('equipment.ejs');
+})
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(1337, function () {
+  console.log("1337 Up and Running Sir!");
+});
